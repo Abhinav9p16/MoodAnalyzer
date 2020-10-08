c81a6d8894d466bfa9c543dd0e3c9fae0dcf38e3
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-10-08 09:19:31 +0530

    Refactor

---
+++ src/test/java/com/cg/moodanalyzer/TestMoodAnalyzer.java
@@ -0,0 +1,17 @@
+package com.cg.moodanalyzer;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestMoodAnalyzer {
+    MoodAnalyzer m;
+    @Test
+    public void test1AnalyzeMoodWhenSad() {
+        m=new MoodAnalyzer("I am in sad mood");
+        Assert.assertEquals(m.analyzeMood(), "Sad");
+    }
+    @Test
+    public void test2AnalyzeMoodWhenNotSad() {
+        m=new MoodAnalyzer("I am in Any mood");
+        Assert.assertEquals(m.analyzeMood(), "Happy");
+    }
+}
